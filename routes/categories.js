f61ea54479f9c32f730eb9d996391bbd2b4d4d40
Anil sharma <smartanil0000@gmail.com>
2023-02-18 12:31:15 +0530

    new things added

--- routes/categories.js
+++ routes/categories.js
@@ -1,29 +1,74 @@
-const {Category} =require('../models/category')
-const express = require('express')
+const { Category } = require("../models/category");
+const express = require("express");
 const router = express.Router();
 
-router.get('/',async (req,res)=>{
-    const categoryList = await Category.find();
+router.get("/", async (req, res) => {
+  const categoryList = await Category.find();
 
-    if(!categoryList){
-        res.status(500).json({success: false})
+  if (!categoryList) {
+    res.status(500).json({ success: false });
+  }
+  res.status(200).send(categoryList);
+});
+
+router.get("/:id", async (req, res) => {
+  const category = await Category.findById(req.params.id);
+  if (!category) {
+    res
+      .status(500)
+      .json({ message: "The category with thr given ID was not found." });
+  }
+  res.status(200).send(category);
+});
+
+router.post("/", async (req, res) => {
+  let category = new Category({
+    name: req.body.name,
+    icon: req.body.icon,
+    color: req.body.color,
+  });
+  category = await category.save();
+
+  if (!category) return res.status(404).send("the category cannot be created!");
+
+  res.send(category);
+});
+
+router.put("/:id", async (req, res) => {
+  const category = await Category.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      icon: req.body.icon,
+      color: req.body.color,
+    },
+    {
+      new: true,
     }
-    res.send(categoryList)
-})
+  );
 
-router.post('/',async (req,res)=>{
-    let category = new Category({
-        name: req.body.name,
-        icon: req.body.icon,
-        color: req.body.icon
+  if (!category) return res.status(400).send("the category cannot be created!");
+
+  res.send(category);
+});
+
+//
+router.delete("/:id", (req, res) => {
+  Category.findByIdAndRemove(req.params.id)
+    .then((category) => {
+      if (category) {
+        return res
+          .status(201)
+          .json({ success: true, message: "the category is deleted..." });
+      } else {
+        return res
+          .status(404)
+          .json({ success: false, message: "category is not found..." });
+      }
     })
-     category = await category.save();
-
-     if(!category)
-     return res.status(404).send('the category cannot be created!')
-
-     res.send(category)
-
-})
+    .catch((err) => {
+      return res.status(400).json({ success: false, error: err });
+    });
+});
 
 module.exports = router;
